06e7162e247a88db9c7a99048842f12a10034933
Ahmed Rashad <ahmedmohamedrashad80@gmail.com>
2024-01-10 07:24:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function toggleDarkMode() {
+  const roots = document.querySelectorAll("*");
+  const changeTheme = document.querySelector(".change");
+
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+  roots.forEach((root) => {
+    if (isDarkMode) {
+      root.classList.add("themes-dark");
+      changeTheme.innerHTML = '<i class="far fa-sun"></i> Light';
+    }
+  });
+
+  changeTheme.addEventListener("click", function () {
+    const newMode = !isDarkMode;
+    roots.forEach((root) => {
+      root.classList.toggle("themes-dark");
+    });
+    localStorage.setItem("darkMode", newMode);
+    if (newMode) {
+      changeTheme.innerHTML = '<i class="far fa-sun"></i> Light';
+    } else {
+      changeTheme.innerHTML = '<i class="far fa-moon"></i> Night';
+    }
+  });
+}
+
+toggleDarkMode();
